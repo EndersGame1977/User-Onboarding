dd86547e6b6a0df92e6e9e7c20f2bd01edbc458c
Jason Prince <jasoncprince@gmail.com>
2019-07-17 14:44:52 -0600

    step 2

--- form/src/OnboardingForm.js
+++ form/src/OnboardingForm.js
@@ -1,18 +1,27 @@
 import React from "react";
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function OnboardingForm() {
+function OnboardingForm({ values }) {
   return (
     <div>
       <Form>
-        <Field type="text" name="name" placeholder="name" />
-        <Field type="email" name="email" placeholder="email" />
-        <Field type="password" name="password" placeholder="password" />
-        <Field
-          type="checkbox"
-          name="termsOfService"
-          placeholder="terms of service"
-        />
+        <div>
+          <ErrorMessage name="name" />
+          <Field type="text" name="name" placeholder="name" />
+        </div>
+        <div>
+          <ErrorMessage name="email" />
+          <Field type="email" name="email" placeholder="email" />
+        </div>
+        <div>
+          <ErrorMessage name="password" />
+          <Field type="password" name="password" placeholder="password" />
+        </div>
+        <label>
+          <Field type="checkbox" name="tos" checked={values.tos} />
+          Accept TOS
+        </label>
         <button>Submit!</button>
       </Form>
     </div>
@@ -20,14 +29,24 @@ function OnboardingForm() {
 }
 
 const FormikOnboardingForm = withFormik({
-  mapPropsToValues({ name, email, password, checkbox }) {
+  mapPropsToValues({ name, email, password, tos }) {
     return {
       name: name || "",
       email: email || "",
       password: password || "",
-      checkbox: checkbox || ""
+      tos: tos || false
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string()
+      .email("Email not valid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be 6 characters or longer")
+      .required("Password is required")
+  }),
 
   handleSubmit(values) {
     console.log(values);
